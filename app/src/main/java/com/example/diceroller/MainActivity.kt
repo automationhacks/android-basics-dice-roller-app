ac4ef37cb8958f41c50393b1bab90e8c03cda676
Gaurav Singh <gaurav.singh@go-jek.com>
2021-10-23 08:36:20 +0530

    Updated MainActivity onCreate method to update text attribute on click on a button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.button)
+        val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
         }
     }
 }
