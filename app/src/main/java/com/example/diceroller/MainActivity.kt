c7359d6556d7e06f7b7e0e883f032363d6e8ab11
Gaurav Singh <gaurav.singh@go-jek.com>
2021-10-23 09:48:04 +0530

    Implemented Dice roll class to generate a new dice roll and update TextView with the result

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,15 +5,36 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows users to roll a dice and view the result on the screen
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
+        // Set a click listener on the button to roll the dice when user taps the button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll().toString()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll
+    }
+}
+
+/**
+ * Dice with fixed no of sides
+ */
+class Dice(private val numSides: Int) {
+    // Generates a random dice roll
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
